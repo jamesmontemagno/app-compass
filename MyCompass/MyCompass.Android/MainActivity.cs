9e6b9d056cbcf54ca4781b7a8d487e9f7d7faccb
James Montemagno <james.montemagno@gmail.com>
2019-02-11 11:25:49 -0800

    Update NuGets

--- MyCompass/MyCompass.Android/MainActivity.cs
+++ MyCompass/MyCompass.Android/MainActivity.cs
@@ -9,7 +9,10 @@ using Android.OS;
 
 namespace MyCompass.Droid
 {
-    [Activity(Label = "MyCompass", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MyCompass", Icon = "@drawable/icon",
+        Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
